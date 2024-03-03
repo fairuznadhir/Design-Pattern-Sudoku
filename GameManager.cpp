0eb8d8986015500d9e8b5fc7964013e14b1331af
JhonDie14 <162027710+JhonDie14@users.noreply.github.com>
2024-03-03 23:59:02 +0700

    Update GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,7 +1,5 @@
 #include "GameManager.h"
 #include <iostream>
 
-GameManager::GameManager(int boardSize) : board(boardSize), currentPlayerIndex(0) {}
-
 void GameManager::startGame() {
     std::cout << "Welcome to Sudoku!" << std::endl;
