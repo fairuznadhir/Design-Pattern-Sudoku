64762d6c629373e4ef742be8ebe821b4cf032963
fairuznadhir <161941434+fairuznadhir@users.noreply.github.com>
2024-03-17 22:42:20 +0700

    GameManager.h

--- GameManager.h
+++ GameManager.h
@@ -7,11 +7,14 @@
 class GameManager {
 private:
     Board board;
+    std::vector<Player> players;
+    int currentPlayerIndex;
 
 public:
     GameManager(int boardSize);
     void startGame();
     bool checkWin();
+    void getPlayerInput(Player& player);
 };
 
 #endif
